7949a55efec89da6c07ac14f21513e6780e14ae7
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2024-04-04 20:32:42 +0300

    Ready for development

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import UserListPage from "./pages/UserListPage/UserListPage";
+import TodoPage from "./pages/TodoPage/TodoPage";
+import HomePage from "./pages/Home/HomePage";
+import NotFoundPage from "./pages/NotFound/NotFoundPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <BrowserRouter>
+      <header>
+        <nav>
+          <ul>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/users'>Users</Link></li>
+            <li><Link to='/todo'>Todo</Link></li>
+          </ul>
+        </nav>
       </header>
-    </div>
+
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/users' element={<UserListPage />} />
+        <Route path='/todo' element={<TodoPage />} />
+        <Route path='/*' element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/UserListPage/UserListPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UserListPage = () => {
+    return (
+        <h1>
+            UserList
+        </h1>
+    );
+}
+
+export default UserListPage;
